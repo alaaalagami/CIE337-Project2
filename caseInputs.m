fe74905e7be7c8944b7f8a01548468c889bb5098
Alaa Alajmy <s-alaaalagami@zewailcity.edu.eg>
2021-05-28 23:16:32 +0300

    Add files via upload

---
+++ caseInputs.m
@@ -0,0 +1,15 @@
+% t and m for case 1 
+t1 = -1/20:1/40:1/20;
+m1 = 5* cos(10*2*pi.*t1);
+
+% t and m for case 2 
+t2 = -1/20:1/20:1/20;
+m2 = 5* cos(10*2*pi.*t2);
+
+% t and m for case 3 
+t3 = -1/20:1/15:1/20;
+m3 = 5* cos(10*2*pi.*t3);
+
+% t and m for case 4 
+t4 = -1/20:1/20:1/20;
+m4 = 5* cos(10*2*pi.*t4);
